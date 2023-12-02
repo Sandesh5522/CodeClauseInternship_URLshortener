a8f7d2dac179b7ab503e9ea508719c8396f9b351
Sandesh <sandeshsutar802@gmail.com>
2023-12-03 03:04:59 +0530

    Final commit.

--- maincode.py
+++ maincode.py
@@ -3,10 +3,10 @@ import urlshortener as us
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-label1 = "Shortened URL will appear here."
+label1 = ""
 layout = [  [sg.Text('Python URL shortener!!')],
             [sg.Text('Enter URL'), sg.InputText('',key='inputURL')],
-            [sg.Text(label1, key='label01')],
+            [sg.Text('Shortened URL will appear here.',key = 'label00'), sg.Text(label1, key = 'label01'), sg.Button('Copy')],
             [sg.Button('Ok'), sg.Button('Cancel')] ]
 
 # Create the Window
@@ -17,7 +17,12 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
     # print('You entered ', values[0])
-    label1 = us.urlshort(values['inputURL'])
-    window['label01'].update("Shortened URL: "+label1)
+    elif event == 'Ok':
+        label1 = us.urlshort(values['inputURL'])
+        window['label00'].update("Shortened URL: ")
+        window['label01'].update(label1)
+    elif event == 'Copy':
+        surl = window['label01'].get()
+        window.TKroot.clipboard_append(surl)
 
 window.close()
--- urlshortener.py
+++ urlshortener.py
@@ -1,11 +1,6 @@
 import pyshorteners
-long_url = input("Enter the URL to shorten: ")
-
 #TinyURL shortener service
 def urlshort(url):
     type_tiny = pyshorteners.Shortener()
     short_url = type_tiny.tinyurl.short(url)
     return short_url
-
-short_url = urlshort(long_url)
-print("The Shortened URL is: " + short_url)
